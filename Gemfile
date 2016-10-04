c59e99cbe93e1f4ecce65844b5ca2eebbea57d8a
Matthew O'Connor <mdavidoconnor@gmail.com>
2016-10-04 16:32:31 -0400

    added datatables

--- Gemfile
+++ Gemfile
@@ -13,7 +13,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
-
+gem 'jquery-datatables-rails', github: 'rweng/jquery-datatables-rails'
+gem 'jquery-ui-rails'
+gem 'roo'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,3 +1,29 @@
 class Equipment < ActiveRecord::Base
   has_many :error_reports
+
+  def self.import_old_equipment
+		Equipment.import(File.open("Book1.xlsx"))
+  end
+
+  def self.import(file)
+		spreadsheet = open_spreadsheet(file)
+		header = spreadsheet.row(1)
+		(2..spreadsheet.last_row).each do |i|
+			#Creates hash keys based on first row names
+			row = Hash[[header, spreadsheet.row(i)].transpose]
+			equipment = find_by_id(row["id"]) || new
+			#Updates attributes of newspaper. NOTE: headers must have the specified names in quotes
+			equipment.update_attributes(name: row["name"])
+			#state_id: State.where(name: row["state"]), county: County.where(name: row["county"]), city: City.where(name: row["city"]),	    
+		end
+	end
+
+  def self.open_spreadsheet(file)
+		case File.extname(file)
+		when ".csv" then Roo::Csv.new(file.path,extension: :csv)
+		when ".xls" then Roo::Excel.new(file.path, extension: :xls)
+		when ".xlsx" then Roo::Excelx.new(file.path, extension: :xlsx)
+		else raise "Unknown file type: #{file.original_filename}"
+		end
+	end
 end
